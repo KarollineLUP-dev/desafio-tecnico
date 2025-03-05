662b195cdaa60024ebef2748698e1a0ff68e710c
Karolline LUP <99025870+KarollineLUP-dev@users.noreply.github.com>
2025-03-05 18:18:58 -0300

    Create desafioTecnico.cs
    Add problema 1

---
+++ desafioTecnico.cs
@@ -0,0 +1,16 @@
+// Problema 1 : Cálculo da variável SOMA
+
+using System;
+
+class Program{
+  static void Main(){
+    int INDICE = 13, SOMA = 0, K = 0;
+
+    while(K < INDICE){
+      K += 1;
+      SOMA += K;
+    }
+
+    Console.WriteLine($"O valo da variável SOMA é: {SOMA}");
+  }
+}
